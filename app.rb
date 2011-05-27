3a80a02972605d8158f80ce9a04b3ddf54a28155
Ben Scheirman <ben@scheirman.com>
2011-05-27 09:21:47 -0500

    Redirect out of www

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ set :haml, :format => :html5
 set :views, File.dirname(__FILE__) + '/www/views'
 set :public, File.dirname(__FILE__) + '/www/public'
 
+before do
+  if request.url =~ /^http:\/\/www\./
+    redirect request.url.gsub(/^http:\/\/www\./, 'http://'), 301
+  end
+end
+
 get '/' do
 	#return "Hello World!"
 	haml :index
