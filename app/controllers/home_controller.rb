393f0af77b998c87e7fc60f7de7b1705f2094487
Ben Scheirman <ben@scheirman.com>
2011-06-17 18:43:34 -0500

    converted to rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def registration
+  end
+
+  def speakers
+  end
+
+  def schedule
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoustonCodeCamp2011::Application.config.secret_token = 'c5fb1001e0f18b47f0ddbc6c4623670890e5e3f8197b9e5dc2e580bf537662d92150edafdc08dbe6a50816230c56c500749fcdd7fead05ab6fed5e54cce4a082'
