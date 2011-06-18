049592900f99db847b01c97d736a4769ed317535
Ben Scheirman <ben@scheirman.com>
2011-06-17 20:16:54 -0500

    Added gravatar for speakers

--- app/helpers/speakers_helper.rb
+++ app/helpers/speakers_helper.rb
@@ -1,2 +1,7 @@
+require 'digest'
+
 module SpeakersHelper
+  def gravatar(speaker)
+    image_tag("http://www.gravatar.com/avatar.php?gravatar_id=#{Digest::MD5::hexdigest(speaker.email)}", :alt => speaker.name, :class => "avatar")
+  end
 end
