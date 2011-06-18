1c5d35134aa22e583dea35f8fd02bd983f2f023c
Ben Scheirman <ben@scheirman.com>
2011-06-18 16:16:07 -0500

    Don't allow editing of speakers unless you're logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,40 +2,40 @@
 # understand the difference!  User sessions are handled in user_sessions_controller.rb
 
 class SessionsController < ApplicationController
-   def new
-     @session = Session.new
-   end
-   
-   def create
-     @session = Session.new(params[:session])
-     if @session.save
-       redirect_to sessions_path, :notice => "Session #{@session.name} created."
-     else
-       flash[:error] = "Bad stuff done happened."
-       render :action => "new"
-     end
-   end
+  before_filter :require_user, :except => [:index, :show]
+  def new
+    @session = Session.new
+  end
 
-   def edit
-     @session = Session.find(params[:id])
-   rescue ActiveRecord::RecordNotFound
-     flash[:error] = "No session found with id #{params[:id]}."
-     redirect_to sessions_path
+  def create
+   @session = Session.new(params[:session])
+   if @session.save
+     redirect_to sessions_path, :notice => "Session #{@session.name} created."
+   else
+     flash[:error] = "Bad stuff done happened."
+     render :action => "new"
    end
-   
-   def update
-     @session = Session.find(params[:id])
-     if @session.update_attributes(params[:session])
-       redirect_to sessions_path, :notice => "Session #{@session.name} updated."
-     else
-       flash[:error] = "Bad stuff done happened."
-       render :action => "edit"
-     end
-   end
-   
-   
-   def index
-     @sessions = Session.all
-   end
+  end
+
+  def edit
+    @session = Session.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = "No session found with id #{params[:id]}."
+    redirect_to sessions_path
+  end
+
+  def update
+    @session = Session.find(params[:id])
+    if @session.update_attributes(params[:session])
+      redirect_to sessions_path, :notice => "Session #{@session.name} updated."
+    else
+      flash[:error] = "Bad stuff done happened."
+      render :action => "edit"
+    end
+  end
+
+  def index
+    @sessions = Session.all
+  end
 end
 
