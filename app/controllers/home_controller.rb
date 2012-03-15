3b27961cf1318347357bf734e3af43985e762daa
Jared <pretender314@gmail.com>
2012-03-14 20:24:46 -0500

    centrallizing sponsors and commenting them out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @sponsors = [
-      # Gold
-      Sponsor.new({:name => 'Microsoft', :url => 'http://www.microsoft.com', :image_path => 'sponsor.jpg', :level => :gold}),
-      Sponsor.new({:name => 'Headspring', :url => 'http://www.headspring.com', :image_path => 'headspring.jpg', :level => :gold}),
-      Sponsor.new({:name => 'ComponentOne', :url => 'http://www.componentone.com', :image_path => 'componentone.jpg', :level => :gold}),
-
-      #Silver
-      Sponsor.new({:name => 'Telerik', :url => 'http://www.telerik.com', :image_path => 'telerik.png', :level => :silver}),
-      Sponsor.new({:name => 'Tekpub', :url => 'http://www.tekpub.com', :image_path => 'tekpub.jpg', :level => :silver}),
-      Sponsor.new({:name => 'Pluralsight', :url => 'http://www.pluralsight.com', :image_path => 'pluralsight.png', :level => :silver}),
-      Sponsor.new({:name => 'Snapstream', :url => 'http://www.snapstream.com', :image_path => 'snapstream.png', :level => :silver}),
-
-      #Special Thanks
-      Sponsor.new({:name => 'Github', :url => 'http://www.github.com', :image_path => 'github.png', :level => :special}),
-      Sponsor.new({:name => 'The Pragmatic Bookshelf', :url => 'http://www.pragprog.com', :image_path => 'pragprog.png', :level => :special}),
-      Sponsor.new({:name => 'Code School', :url => 'http://www.codeschool.com', :image_path => 'codeschool.png', :level => :special}),
-      Sponsor.new({:name => "O'Reilly", :url => 'http://www.oreilly.com', :image_path => 'oreilly.png', :level => :special}),
-      Sponsor.new({:name => 'Manning Publications', :url => 'http://www.manning.com', :image_path => 'manning.png', :level => :special}),
-      Sponsor.new({:name => 'ChaiONE', :url => 'http://www.chaione.com', :image_path => 'chaione.png', :level => :special}),
-      Sponsor.new({:name => 'LogicNP Software', :url => 'http://www.logicnp.com', :image_path => 'logicnp.jpg', :level => :special})
-    ]    
+    @sponsors = get_sponsors
   end
 
   def registration
@@ -46,8 +26,12 @@ class HomeController < ApplicationController
   end
 
   def sponsors
+    @sponsors = get_sponsors
+  end
+
     # Todo: replace with db
-    @sponsors = [
+  def get_sponsors
+    [
       # Gold
       #Sponsor.new({:name => 'Microsoft', :url => 'http://www.microsoft.com', :image_path => 'sponsor.jpg', :level => :gold}),
       #Sponsor.new({:name => 'Headspring', :url => 'http://www.headspring.com', :image_path => 'headspring.jpg', :level => :gold}),
@@ -67,6 +51,7 @@ class HomeController < ApplicationController
       #Sponsor.new({:name => 'Manning Publications', :url => 'http://www.manning.com', :image_path => 'manning.png', :level => :special}),
       #Sponsor.new({:name => 'ChaiONE', :url => 'http://www.chaione.com', :image_path => 'chaione.png', :level => :special}),
       #Sponsor.new({:name => 'LogicNP Software', :url => 'http://www.logicnp.com', :image_path => 'logicnp.jpg', :level => :special})
-    ]
+    ]    
   end
+
 end
