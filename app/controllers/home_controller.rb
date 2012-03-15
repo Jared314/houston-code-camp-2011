f0642b4757e6cf6991fb6482dfcfdc4ffeea5bb3
Jared <pretender314@gmail.com>
2012-03-14 20:14:19 -0500

    simplifying index page to draw from a collection

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,26 @@
 class HomeController < ApplicationController
   def index
+    @sponsors = [
+      # Gold
+      Sponsor.new({:name => 'Microsoft', :url => 'http://www.microsoft.com', :image_path => 'sponsor.jpg', :level => :gold}),
+      Sponsor.new({:name => 'Headspring', :url => 'http://www.headspring.com', :image_path => 'headspring.jpg', :level => :gold}),
+      Sponsor.new({:name => 'ComponentOne', :url => 'http://www.componentone.com', :image_path => 'componentone.jpg', :level => :gold}),
+
+      #Silver
+      Sponsor.new({:name => 'Telerik', :url => 'http://www.telerik.com', :image_path => 'telerik.png', :level => :silver}),
+      Sponsor.new({:name => 'Tekpub', :url => 'http://www.tekpub.com', :image_path => 'tekpub.jpg', :level => :silver}),
+      Sponsor.new({:name => 'Pluralsight', :url => 'http://www.pluralsight.com', :image_path => 'pluralsight.png', :level => :silver}),
+      Sponsor.new({:name => 'Snapstream', :url => 'http://www.snapstream.com', :image_path => 'snapstream.png', :level => :silver}),
+
+      #Special Thanks
+      Sponsor.new({:name => 'Github', :url => 'http://www.github.com', :image_path => 'github.png', :level => :special}),
+      Sponsor.new({:name => 'The Pragmatic Bookshelf', :url => 'http://www.pragprog.com', :image_path => 'pragprog.png', :level => :special}),
+      Sponsor.new({:name => 'Code School', :url => 'http://www.codeschool.com', :image_path => 'codeschool.png', :level => :special}),
+      Sponsor.new({:name => "O'Reilly", :url => 'http://www.oreilly.com', :image_path => 'oreilly.png', :level => :special}),
+      Sponsor.new({:name => 'Manning Publications', :url => 'http://www.manning.com', :image_path => 'manning.png', :level => :special}),
+      Sponsor.new({:name => 'ChaiONE', :url => 'http://www.chaione.com', :image_path => 'chaione.png', :level => :special}),
+      Sponsor.new({:name => 'LogicNP Software', :url => 'http://www.logicnp.com', :image_path => 'logicnp.jpg', :level => :special})
+    ]    
   end
 
   def registration
