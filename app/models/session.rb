4018c3f5df8a1716c1d2dd3862355c76385a1915
Ben Scheirman <ben@scheirman.com>
2012-07-29 23:37:02 -0500

    better session unassigned/assigned fetching

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,7 @@
 class Session < ActiveRecord::Base
   belongs_to :speaker
+  validates :slot_key, :length => { :minimum => 1 }
+
+  scope :unassigned, where("slot_key = '' OR slot_key IS NULL")
+  scope :assigned, where("slot_key != '' AND slot_key IS NOT NULL")
 end
